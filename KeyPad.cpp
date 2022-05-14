cc83b6ae548b123fcc8155dc5d22d58365ce01da
AidenTemere <22532064+SheriffEllis@users.noreply.github.com>
2022-05-14 17:35:27 +0100

    Update KeyPad
    KeyPad class updated to account for timeouts

--- KeyPad.cpp
+++ KeyPad.cpp
@@ -10,7 +10,13 @@ void KeyPad::flushSerial(){
 
 void KeyPad::waitForInput(){
     while(!Serial.available()){
-        // wait for input
+        // wait for input without a timeout
+    }
+}
+
+void KeyPad::waitForInput(unsigned long timer_start){
+    while(!Serial.available() and (millis() - timer_start) < TIMEOUT){
+        // wait for input with a timeout
     }
 }
 
@@ -23,7 +29,7 @@ int KeyPad::getPin(){
         waitForInput();
         pin = Serial.parseInt();
         if(pin == 0 or pin > 9999){
-            Serial.println("Error: Please input only 4 digits and no letters or characters.");
+            Serial.println("Error: Invalid pin. Please input only 4 digits and no letters or characters.");
             Serial.println("The pin 0000 will not be accepted.");
         }else{
             valid = true;
@@ -33,21 +39,45 @@ int KeyPad::getPin(){
     return pin;
 }
 
-int KeyPad::getChoice(String choices[], int maxChoiceNum){
+int KeyPad::getPin(unsigned long timer_start){
+    bool valid = false;
+    int pin = 0;
+    while(!valid){
+        flushSerial();
+        Serial.println("Please input a 4-digit pin:");
+        waitForInput(timer_start);
+        if((millis() - timer_start) >= TIMEOUT){
+            Serial.println("TIMEOUT EXCEEDED");
+            flushSerial();
+            return -1; // -1 represents failed pin entry
+        }
+        pin = Serial.parseInt();
+        if(pin == 0 or pin > 9999){
+            Serial.println("Error: Invalid pin. Please input only 4 digits and no letters or characters.");
+            Serial.println("The pin 0000 will not be accepted.");
+        }else{
+            valid = true;
+        }
+    }
+    flushSerial();
+    return pin;
+}
+
+int KeyPad::getChoice(String choices[], int max_choice_num){
     bool valid = false;
     char choice;
     while(!valid){
         flushSerial();
-        for (int i = 0; i <= maxChoiceNum; i++){
+        for (int i = 0; i <= max_choice_num; i++){
             Serial.println(choices[i]);
         }
         Serial.print("Choose an option by inputting a digit from 0 to ");
-        Serial.println(maxChoiceNum);
+        Serial.println(max_choice_num);
         waitForInput();
         choice = Serial.read();
-        if(choice < '0' or choice > '0' + maxChoiceNum){
+        if(choice < '0' or choice > '0' + max_choice_num){
             Serial.print("Error: Please input a SINGLE digit from 0 to ");
-            Serial.println(maxChoiceNum);
+            Serial.println(max_choice_num);
         }else{
             valid = true;
         }
--- KeyPad.h
+++ KeyPad.h
@@ -5,11 +5,14 @@
 
 class KeyPad{
     private:
+        const static unsigned long TIMEOUT = 20000;
         void flushSerial();
         void waitForInput();
+        void waitForInput(unsigned long timer_start);
     public:
         int getPin();
-        int getChoice(String choices[], int maxChoiceNum);
+        int getPin(unsigned long timer_start);
+        int getChoice(String choices[], int max_choice_num);
 };
 
 #endif
