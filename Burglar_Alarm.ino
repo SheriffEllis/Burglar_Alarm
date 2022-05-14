e7e47a18c77a9f896ae42b90da6842da66334afe
AidenTemere <22532064+SheriffEllis@users.noreply.github.com>
2022-05-14 19:03:32 +0100

    Mag switch and Solenoid Unit test

--- Burglar_Alarm.ino
+++ Burglar_Alarm.ino
@@ -1,19 +1,24 @@
-#include "Logger.h"
+#include "MagneticSwitch.h"
+#include "LED.h"
+#include "Solenoid.h"
 
-Logger logger;
+LED led(10);
+Solenoid solenoid(9);
+MagneticSwitch magnetic_switch1(12);
+MagneticSwitch magnetic_switch2(11);
+
 
 void setup(){
   Serial.begin(9600);
-  logger.printLog();
-  Serial.println("-------------------");
-  logger.logEvent(1);
-  delay(1000);
-  logger.logEvent(2);
-  delay(1500);
-  logger.logEvent(3);
-  logger.printLog();
+  pinMode(13, OUTPUT);
+  digitalWrite(13, LOW);
 }
 
 void loop(){
+  led.setState(magnetic_switch1.measure());
+  if(magnetic_switch2.measure()){
+    solenoid.open(1000);
+  }
 
+  solenoid.update();
 }
