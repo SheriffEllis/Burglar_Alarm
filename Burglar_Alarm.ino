4185093ef867e716a16d2cf12dadda079c3f0eed
AidenTemere <22532064+SheriffEllis@users.noreply.github.com>
2022-05-14 23:45:03 +0100

    Controller class now able to be tested in main

--- Burglar_Alarm.ino
+++ Burglar_Alarm.ino
@@ -1,17 +1,22 @@
 #include "Arduino.h"
-#include "Buzzer.h"
+#include "Controller.h"
 
-Buzzer buzzer(10, 9);
+#define ARMED_LED_PIN 12
+#define TRIGGERED_LED_PIN 11
+#define LOUD_BUZZ_PIN 10
+#define QUIET_BUZZ_PIN 9
+#define SOLENOID_PIN 8
+#define MAGSWITCH_PIN 7
+#define PIR_PIN 6
+
+Controller controller(ARMED_LED_PIN, TRIGGERED_LED_PIN, 
+    LOUD_BUZZ_PIN, QUIET_BUZZ_PIN, SOLENOID_PIN, MAGSWITCH_PIN, PIR_PIN);
 
 void setup(){
-  Serial.begin(9600);
-  pinMode(13, OUTPUT);
-  digitalWrite(13, LOW);
 }
 
 void loop(){
-  buzzer.pulse(1000);
-  delay(500);
-  buzzer.pulse(2000,true);
-  delay(3000);
+  controller.processSysState();
+  controller.updateSensors();
+  controller.updateTimers();
 }
--- Controller.cpp
+++ Controller.cpp
@@ -20,12 +20,23 @@ Controller::Controller(int armed_LED_pin, int triggered_LED_pin, int loud_buzz_p
         magnetic_switch(magswitch_pin),
         pir(PIR_pin)
 {
+    Serial.begin(9600);
     system_state = 0;
 }
 
-// TODO
 void Controller::armAlarm(){
-
+    armed_LED.setState(true);
+    Serial.println("Arming alarm system...\n1 minute countdown begun");
+    buzzer.setTone(400);
+    for (int i = 60; i >= 0; i--)
+    {
+        Serial.print(i);
+        Serial.println("s");
+        buzzer.pulse(500, true); // quiet short pulses
+        delay(1000);
+    }
+    buzzer.setTone(1000);
+    buzzer.pulse(1000); // longer loud pulse
 }
 
 // TODO
@@ -45,31 +56,77 @@ void Controller::sendAlert(int event_type){
 }
 
 // TODO
+/*
+ *  0 - Initial state
+ *  1 - Alarm disabled, waiting on user to log in
+ *  3 - Main menu
+ *  4 - Arm Alarm
+ *  6 - Armed state
+ *  7 - Sensors triggered OR Facial recognition success
+ *  10 - Alarm triggered: Set off all alerts
+ *  11 - Waiting for PIN in triggered state
+ *  13 - Check sensors
+ *  14 - System Settings
+ *  15 - Setting new pin
+ *  16 - Setting facial recognition
+ */
 void Controller::processSysState(){
     switch(system_state){
         case 0: // Initial state
-            Serial.println("System unarmed");
-            resetAlarm();
-            system_state = 1; // Alarm disabled waiting on user to log in
-            break;
+            {
+                Serial.println("System unarmed");
+                resetAlarm();
+                system_state = 1; // Alarm disabled waiting on user to log in
+            }break;
 
         case 1: // Alarm disabled, waiting on user to log in
-            Serial.println("Pin must be re-entered to continue");
-            Serial.println("No limit to number of attempts.");
-            if(pin_handler.verifyPin(keypad.getPin())){
-                system_state = 2; // Enter main menu
-            };
-            pin_handler.resetTries();
-            break;
+            {
+                Serial.println("Pin must be re-entered to continue");
+                Serial.println("No limit to number of attempts.");
+                if(pin_handler.verifyPin(keypad.getPin())){
+                    system_state = 3; // Main menu
+                };
+                pin_handler.resetTries();
+            }break;
 
-        case 2: // Main menu
-            
-            break;
+        case 3: // Main menu
+            {
+                int choice = keypad.getChoice("0 - Back\n1 - System Setting\n2 - Check Sensors\n3 - Arm Alarm", 3);
+                switch(choice){
+                    case 0:
+                        system_state = 0; // Initial state
+                        break;
+                    case 1:
+                        system_state = 14; // System Settings
+                        break;
+                    case 2:
+                        system_state = 13; // Check Sensors
+                        break;
+                    case 3:
+                        system_state = 4; // Arm Alarm
+                        break;
+                    default:
+                        system_state = 0;
+                        break;
+                }
+            }break;
+        
+        case 4: // Arm Alarm
+            {
+                armAlarm();
+                system_state = 6; // Armed state
+            }break;
+
+        case 6: // Armed state
+            {
+                // Check on sensors and facial recognition
+            }break;
 
         default:
-            Serial.println("Invalid state detected, resetting system");
-            system_state = 0;
-            break;
+            {
+                Serial.println("Invalid state detected, resetting system");
+                system_state = 0;
+            }break;
     }
 }
 
